b49a0f8bac8d1c9da6e43b2779e8c7b8a7f5762d
Deiondre Andrews <andrews.deiondre@gmail.com>
2020-12-22 09:45:16 -0500

    Add example for organizing tests into a suite

---
+++ src/test/java/com/syntechx/junit/helper/TestSuite.java
@@ -0,0 +1,9 @@
+package com.syntechx.junit.helper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ QuickBeforeAfterTest.class, StringHelperTest.class })
+public class TestSuite {}
