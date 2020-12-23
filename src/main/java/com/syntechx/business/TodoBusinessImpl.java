f6a1205e43d9170bbae134ab574042e259085650
Deiondre Andrews <andrews.deiondre@gmail.com>
2020-12-22 22:33:31 -0500

    Fix formatting in some files

--- src/main/java/com/syntechx/business/TodoBusinessImpl.java
+++ src/main/java/com/syntechx/business/TodoBusinessImpl.java
@@ -12,9 +12,9 @@ import com.syntechx.data.api.TodoService;
 public class TodoBusinessImpl {
 
     private TodoService todoService;
-
-	public TodoBusinessImpl(TodoService todoService) {
-		this.todoService = todoService;
+    
+    public TodoBusinessImpl(TodoService todoService) {
+        this.todoService = todoService;
     }
     
     public List<String> retrieveTodosRelatedToSpring(String user) {
--- src/test/java/com/syntechx/data/api/TodoServiceStub.java
+++ src/test/java/com/syntechx/data/api/TodoServiceStub.java
@@ -12,5 +12,5 @@ public class TodoServiceStub implements TodoService {
             "Learn Spring",
             "Learn to Dance"
         );
-	}
+    }
 }
